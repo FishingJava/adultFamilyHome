6bba52d7cff47b4910563bcda95d663dea9d6c25
Fish <52393106+FishingJava@users.noreply.github.com>
2025-10-23 13:28:52 -0700

    initial commit on Adult family Home website

---
+++ src/main/java/com/adfh/Br/adultFamilyHome/AdultFamilyHomeApplication.java
@@ -0,0 +1,14 @@
+package com.adfh.Br.adultFamilyHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdultFamilyHomeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AdultFamilyHomeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/adfh/Br/adultFamilyHome/controller/ContactController.java
@@ -0,0 +1,38 @@
+package com.adfh.Br.adultFamilyHome.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+public class ContactController {
+
+    private final EmailNotificationService emailService;
+
+    @PostMapping("/contact")
+    public String handleContactForm(@RequestBody Map<String, String> formData){
+        String name = formData.get("name");
+        String email = formData.get("email");
+        String phone = formData.get("phone");
+        String message = formData.get("message");
+
+        String subject = "New Contact Message from " + name;
+        String body = "Name: " + name +  "\nPhone: " + phone + "\nEmail: " + email + "\n\nMessage:\n" + message;
+
+        // Send the email to your admin inbox
+        emailService.send("fishingofjava@gmail.com", subject, body);
+
+        // Optional: send confirmation email to the user
+        emailService.send(email,
+                "Thank you for contacting Silver Oak -Nathen AFH \n",
+                "Hello, " + name + ",\n" + "We've received your message and will get back to you soon.\n" + "\nThank you!" + "\nSilver Oak - Nathen Adult Family Home");
+
+
+        return "redirect:/?success=true";
+    }
+}
